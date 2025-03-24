85de29818e9d140480a6c24f6904656abede017f
yeastofficial <Yeast2018official@gmail.com>
2025-03-24 19:01:58 +0545

    Create floating-popup.js

---
+++ floating-popup.js
@@ -0,0 +1,5 @@
+// JavaScript to trigger floating popup animation on page load
+window.onload = function () {
+    const popup = document.querySelector('.popup');
+    popup.style.animation = "floatIn 1.5s ease-out forwards";
+};
